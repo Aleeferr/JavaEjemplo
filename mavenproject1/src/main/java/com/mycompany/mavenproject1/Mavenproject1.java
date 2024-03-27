510630a65c15379159e81292134fc4b347444755
Aleeferr <richarriega18@gmail.com>
2024-03-26 22:51:38 -0300

    metodo constructor

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -10,8 +10,38 @@ package com.mycompany.mavenproject1;
  * @author Alumno
  */
 public class Mavenproject1 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private String nombre;
+    private String apellido;
+    private int edad;
+    
+    public Persona (String nombre, String apellido) {
+    
+    public String saludar () {
+        return "Hola soy" + nombre "  mi apellido es " + apellido + " tengo " + edad;
     }
-}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    }
+
